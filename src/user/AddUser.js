92f41c3bab97053981f06f3e51f029a19a9a01c2
OmerGit <rafaelxdale@gmail.com>
2023-02-09 00:18:41 -0500

    update

--- src/user/AddUser.js
+++ src/user/AddUser.js
@@ -1,13 +1,13 @@
 import React, {useState} from "react";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {useNavigate, Link} from "react-router-dom";
 
 export default function AddUser() {
     let navigate = useNavigate();
     const [user, setUser] = useState({
         name: "",
         username: "",
-        email: ""
+        email: "",
     });
     const {name, username, email} = user;
     const onInputChange = (e) => {
@@ -18,21 +18,20 @@ export default function AddUser() {
         await axios.post("http://localhost:3004/user", user);
         navigate("/");
     };
+
     return (
         <div className={"container"}>
             <div><br/></div>
-            <div className="row">
-                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                    <h2 className="text-center m-4">
-                        Register User
-                    </h2>
+            <div className={"row"}>
+                <div className={"col-md-6 offset-md-3 border rounded p-4 mt-2 shadow"}>
+                    <h2 className={"text-center m-4"}>Register User</h2>
                     <form onSubmit={(e) => onSubmit(e)}>
-                        <div className="mb-3">
+                        <div className={"mb-3"}>
                             <label htmlFor="Name" className={"form-label"}>Name</label>
                             <input type="text"
                                    className={"form-control"}
                                    placeholder={"Enter your name"}
-                                   name={"Name"}
+                                   name={"name"}
                                    value={name}
                                    onChange={(e) => onInputChange(e)}/>
                         </div>
@@ -41,7 +40,7 @@ export default function AddUser() {
                             <input type="text"
                                    className={"form-control"}
                                    placeholder={"Enter your last name"}
-                                   name={"Last Name"}
+                                   name={"username"}
                                    value={username}
                                    onChange={(e) => onInputChange(e)}/>
                         </div>
@@ -50,12 +49,15 @@ export default function AddUser() {
                             <input type="email"
                                    className={"form-control"}
                                    placeholder={"Enter your e-mail address"}
-                                   name={"Email"}
+                                   name={"email"}
                                    value={email}
                                    onChange={(e) => onInputChange(e)}/>
                         </div>
-                        <button type={"submit"} className={"btn btn-outline-primary"}>Submit</button>
-                        <button type={"submit"} className={"btn btn-outline-danger mx-3"}>Submit</button>
+                        <button type={"submit"}
+                                className={"btn btn-outline-primary"}>Submit
+                        </button>
+                        <Link to={"/"}
+                              className={"btn btn-outline-danger mx-3"}>Cancel</Link>
                     </form>
                 </div>
             </div>
