affbf93ada1e5db9f0b37a25de69ce7387fca667
OmerGit <rafaelxdale@gmail.com>
2023-02-08 21:31:33 -0500

    Initial commit

---
+++ src/layout/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <h1>no esta funcionando del todo bien </h1>
+            <div className="container-fluid">
+                {/*<a className="navbar-brand" >HOME</a>*/}
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+    )
+}
